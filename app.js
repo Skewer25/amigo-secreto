30cf1aec0307a18d7c021b10ff6e9703f1994b8b
Skewers <sskewer7@gmail.com>
2025-08-04 21:52:00 -0400

    actualizando el app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = []
+
+function agregarAmigo(nombre) {
+    // Verifica si el nombre es una cadena de texto no vacía
+  nombre = document.getElementById('amigo').value;
+  if (typeof nombre === 'string' && nombre.trim() !== '') {
+    listaDeAmigos.push(nombre);
+  } else {
+    alert('Por favor, inserte un nombre.');
+  }
+  limpiarCaja();
+  mostrarLista();
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function mostrarLista() {
+    let listaHTML = document.querySelector('#listaAmigos');
+    listaHTML.innerHTML = ''; //limpiar la lista antes de mostrar
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        // Crear un elemento <li> para cada amigo
+        let li = document.createElement('li');
+        li.textContent = listaDeAmigos[i];
+        listaHTML.appendChild(li);
+    }
+}
+
